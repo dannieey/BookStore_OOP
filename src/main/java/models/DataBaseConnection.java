11b9872f9efda51c38a3a6a43ee2e52b410e29fc
Danel <danel.li@mail.ru>
2025-02-12 21:31:08 +0500

    DB_Connection

---
+++ src/main/java/models/DataBaseConnection.java
@@ -0,0 +1,28 @@
+package models;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DataBaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/BookStore";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "0000";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            // Регистрируем драйвер вручную
+            Class.forName("org.postgresql.Driver");
+
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new SQLException("Failed to establish connection to the database");
+        }
+    }
+
+
+}
+
