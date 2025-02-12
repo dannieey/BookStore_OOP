5b4013da306c8f9d2540159f47d3f91a10378ffc
Nurdana <241349@astanait.edu.kz>
2025-02-12 22:15:28 +0500

    save to Database

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -15,5 +15,27 @@ public class Users {
         this.name = name;
         this.surname = surname;
         this.book = null;
+        saveToDatabase();
+    }
+
+    private void saveToDatabase() {
+        String query = "INSERT INTO users (name, surname) VALUES (?, ?)";
+
+        try (Connection conn = DataBaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            pstmt.setString(1, name);
+            pstmt.setString(2, surname);
+            pstmt.executeUpdate();
+
+            ResultSet rs = pstmt.getGeneratedKeys();
+            if (rs.next()) {
+                this.id = rs.getInt(1);
+            }
+
+            System.out.println("\nUser: " + name + " " + surname + " have been successfully added to the database.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
